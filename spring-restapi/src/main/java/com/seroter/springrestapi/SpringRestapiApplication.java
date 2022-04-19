507acf3bb90c176925c64910ae01e30cd755b006
Biju Kunjummen <biju.kunjummen@gmail.com>
2022-04-19 16:30:07 -0700

    Upgrades Java version to 17

--- spring-restapi/src/main/java/com/seroter/springrestapi/SpringRestapiApplication.java
+++ spring-restapi/src/main/java/com/seroter/springrestapi/SpringRestapiApplication.java
@@ -1,38 +1,28 @@
 package com.seroter.springrestapi;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 @RestController
 @SpringBootApplication
 public class SpringRestapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringRestapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestapiApplication.class, args);
+    }
 
-	List<Employee> employees;
+    private final List<Employee> employees = List.of(
+            new Employee("300", "Walt Longmire", "WYG", "Sheriff"),
+            new Employee("301", "Vic Moretti", "WYG", "Deputy")
+    );
 
-	public SpringRestapiApplication() {
-
-		employees = new ArrayList<Employee>();
-		employees.add(new Employee("300", "Walt Longmire", "WYG", "Sheriff"));
-		employees.add(new Employee("301", "Vic Moretti", "WYG", "Deputy"));
-
-	}
-
-	@GetMapping("/employees")
-	public Flux<Employee> getAllEmployees() {
-		return Flux.fromIterable(employees);
-	}
-
-
-
+    @GetMapping("/employees")
+    public Flux<Employee> getAllEmployees() {
+        return Flux.fromIterable(employees);
+    }
 }
